83eab477872def2d714ab21ba56742f9d0ee050b
hehuapei <hehuapei@qq.com>
2020-04-21 15:45:07 +0800

    rename

---
+++ README.md
@@ -0,0 +1,8 @@
+这是个存放分享学习demo的项目
+
+对应文章可以到[https://www.hehuapei.com](https://www.hehuapei.com)上面查看
+
+
+#### Project
+---
+- 1. flutter实现应用内升级demo: 基于flutter的APP内升级简单实现;[Link](https://www.hehuapei.com)
--- flutter-app-update-demo/app_update_demo/lib/main.dart
+++ flutter-app-update-demo/app_update_demo/lib/main.dart
@@ -84,7 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
     Map map = new HashMap();
     map['data'] = '1.新增app应用内升级\n2.修复若干个bug';
     map['ver'] = '1.0.1';
-    map['url'] = 'https://github.com/hehuapei/flutter-demo/raw/master/resource/app.apk';
+    map['url'] = 'https://github.com/hehuapei/study-demo/raw/master/flutter-app-update-demo/resource/app.apk';
 
     //判断如果服务器上版本比当前版本新,则返回最新版本信息
     if((Random().nextInt(10)) > 3){
